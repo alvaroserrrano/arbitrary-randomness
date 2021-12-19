6fbd0256023d0744a0efe06fbbf8b0d17b7363dd
Alvaro Serrano <alvaro.serrano@wmich.edu>
2021-12-19 20:29:59 +0100

    wallet connect

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,19 @@ import { RecoilRoot } from 'recoil';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-
+import { Web3ReactProvider } from '@web3-react/core';
+import Web3 from 'web3';
+import { provider } from 'web3-core';
+function getLibrary(provider: provider) {
+  return new Web3(provider);
+}
 ReactDOM.render(
   <React.StrictMode>
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
+    </Web3ReactProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
